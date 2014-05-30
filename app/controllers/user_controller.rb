0abace69880011c7fe0a5ad481317f951570d779
Justin Stewart <email": null>
2014-05-30 12:47:05 -0500

    User Views added

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -2,37 +2,40 @@ class UserController < ApplicationController
   def index
      @user = User.find(session[:user_id])
       puts "google" * 50
-     @user.client.connections.all.each do |connect|
 
-
+    @user.client.connections.all.each do |connect|
       name = connect.first_name + " " + connect.last_name
-      location = connect.location.name
+      if connect.location
+        location = connect.location.name
+      end
       industry =  connect.industry
-      img_url = @user.client.picture_urls(:id => connect.id)
-      p "hah" * 20
-      p [name, location, industry, img_url.all.first, connect.id].inspect
-      p "raa" * 20
+      # img_url = @user.client.picture_urls(:id => connect.id)
+      img_url = connect.picture_url
+    # p "hah" * 20
+    # p [name, location, industry, img_url, connect.id]
+    # p "raa" * 20
       @user.connections.create(
         location: location,
         industry: industry,
         name: name,
-        img_url: img_url.all.first,
+        img_url: img_url,
         connection_id: connect.id
       )
 
     end
 
-    # puts @user.client.connections.length
-    # p "+" * 50
-    # puts @user.client.connections.last
-    # p "-" * 50
+# ---------------------PUT IN MODEL-----------------------------
+
+    all_locations = []
+    all_industries =[]
+    @user.connections.each do |connect|
+      all_locations << connect.location
+      all_industries << connect.industry
+    end
+
+    @unique_locations = all_locations.uniq.compact  # RESOLVES UNIQUENESS and NIL values
+    @unique_industries = all_industries.uniq.compact
+
 
   end
 end
-# <% @user.client.connections.each do |connect| %>
-# <ul>
-#   <li>
-#     <%= connect.picture_url %>
-#   </li>
-# </ul>
-# <%end%>
